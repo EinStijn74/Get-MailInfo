71c5048fe3c10019d7efb49813ab35edd4fecc61
EinStijn74 <stijn@rommelkamer.nl>
2017-05-19 10:43:28 +0200

    Update Get-MailInfo.ps1
    Added error handling and main Exchange attributes

--- Get-MailInfo.ps1
+++ Get-MailInfo.ps1
@@ -1,50 +1,82 @@
- function Get-MailInfo {
+function Get-MailboxInfo {
 
-################################################################################################
-# Name: Get-MailInfo                                                                           #
-# Version: 1.0 (May, 2017)                                                                     #
-# Created by: Stijn Langenberg                                                                 #
-# What is it: Script combines output for Get-Mailbox, Get-MAilboxStatics and Get-CasMailbox    #
-################################################################################################
+    ##############################################################################################
+    # Name: Get-MailboxInfo                                                                      #
+    # Version: 1.01 (May, 2017)                                                                   #
+    # Created: Stijn Langenberg                                                                  #
+    # What is it: Script combines output for Get-Mailbox, Get-MAilboxStatics and Get-CasMailbox  #
+    ##############################################################################################
 
-　
-#Function accepts pipeline input from Get-Aduser, Get-Mailbox, etc (property: SamSccountName)
-[CmdletBinding()]            
- Param             
-  (                       
-  [Parameter(
-   Mandatory=$true,
-   ValueFromPipeline=$true,
-   ValueFromPipelineByPropertyName=$true)]
-  [AllowEmptyCollection()]
-  [String[]]
-  $SamAccountName
-  )
 
-Process {
+    #Function accepts pipeline input from Get-Aduser, Get-Mailbox, etc (property: SamSccountName)
+    [CmdletBinding()]            
+    Param             
+    (                       
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [AllowEmptyCollection()]
+        [String[]]
+        $SamAccountName
+    )
 
-$Output = @()
+    Process {
 
-  ForEach ($User in $SamAccountName) {
-  $Mailbox = Get-Mailbox $User
-  $MailboxStatistics = Get-MailboxStatistics $User
-  $CasMailbox = Get-CASMailbox $user
+        $Output = @()
 
-   #Below Properties are an example more can be added
-   $Property = "" | Select-Object DisplayName,SamAccountName,ItemCount,DatabaseProhibitSendReceiveQuota,TotalItemSize,DeletedItemCount
-     $Property.Displayname = $Mailbox.DisplayName
-     $Property.SamAccountName = $Mailbox.SamAccountName
-     $Property.ItemCount = $MailboxStatistics.ItemCount
-     $Property.DatabaseProhibitSendReceiveQuota = $MailboxStatistics.DatabaseProhibitSendReceiveQuota
-     $Property.TotalItemSize = $MailboxStatistics.TotalItemSize
-     $Property.DeletedItemCount = $MailboxStatistics.DeletedItemCount
+        ForEach ($User in $SamAccountName) {
+            if (!($Mailbox = Get-Mailbox $User -ErrorAction SilentlyContinue)) { 
+                Write-Warning "Cannot find Exchange information for $user"            
+            }#if
+            Else {
+                $Mailbox = Get-Mailbox $User
+                $MailboxStatistics = Get-MailboxStatistics $User
+                $CasMailbox = Get-CASMailbox $user
+
+                #More properties can be added below
+                $Property = "" | Select-Object DisplayName, Name, SamAccountName, UserPrincipalName, OrganizationalUnit, Alias, PrimarySmtpAddress, ServerName, Database, 
+                ItemCount, TotalItemSize, IssueWarningQuota, ProhibitSendQuota, ProhibitSendReceiveQuota, ArchiveWarningQuota, ArchiveQuota, RulesQuota, UseDatabaseQuotaDefaults,
+                DatabaseProhibitSendReceiveQuota, DatabaseIssueWarningQuota, DatabaseProhibitSendQuota, DeletedItemCount, TotalDeletedItemSize, RetentionPolicy, OWAEnabled,
+                ActiveSyncEnabled, OwaMailboxPolicy
+
+                $Property.Displayname = $Mailbox.DisplayName
+                $Property.Name = $Mailbox.Name
+                $Property.SamAccountName = $Mailbox.SamAccountName
+                $Property.UserPrincipalName = $Mailbox.UserPrincipalName
+                $Property.OrganizationalUnit = $Mailbox.OrganizationalUnit
+                $Property.Alias = $Mailbox.Alias
+                $Property.PrimarySmtpAddress = $Mailbox.PrimarySmtpAddress
+                $Property.ServerName = $Mailbox.ServerName
+                $Property.Database = $Mailbox.Database
+                $Property.ItemCount = $MailboxStatistics.ItemCount
+                $Property.TotalItemSize = $MailboxStatistics.TotalItemSize
+                $Property.IssueWarningQuota = $Mailbox.IssueWarningQuota
+                $Property.ProhibitSendQuota = $Mailbox.ProhibitSendQuota
+                $Property.ProhibitSendReceiveQuota = $Mailbox.ProhibitSendReceiveQuota
+                $Property.ArchiveWarningQuota = $Mailbox.ArchiveWarningQuota
+                $Property.ArchiveQuota = $Mailbox.ArchiveQuota
+                $Property.RulesQuota = $Mailbox.RulesQuota
+                $Property.UseDatabaseQuotaDefaults = $Mailbox.UseDatabaseQuotaDefaults
+                $Property.DatabaseProhibitSendReceiveQuota = $MailboxStatistics.DatabaseProhibitSendReceiveQuota
+                $Property.DatabaseIssueWarningQuota = $MailboxStatistics.DatabaseIssueWarningQuota
+                $Property.DatabaseProhibitSendQuota = $MailboxStatistics.DatabaseProhibitSendQuota
+                $Property.DeletedItemCount = $MailboxStatistics.DeletedItemCount
+                $Property.TotalDeletedItemSize = $MailboxStatistics.TotalDeletedItemSize
+                $Property.UseDatabaseQuotaDefaults = $Mailbox.UseDatabaseQuotaDefaults
+                $Property.RetentionPolicy = $Mailbox.RetentionPolicy
+                $Property.OWAEnabled = $CasMailbox.OWAEnabled
+                $Property.ActiveSyncEnabled = $CasMailbox.ActiveSyncEnabled
+                $Property.OwaMailboxPolicy = $CasMailbox.OwaMailboxPolicy
   
-   $Output += $Property
+                $Output += $Property
+            }#Else
+        }#ForEach
 
-  }#ForEach
+        $Output
 
-$Output
+    }#Process
 
-}#Process
+}#function
 
-}#function 
+
